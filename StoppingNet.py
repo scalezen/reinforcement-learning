30def58797abf4437cd9926f6ecf646b96033ca4
Radhika Saksena <6136756+scalezen@users.noreply.github.com>
2026-08-24 23:11:13 +0100

    linting.

--- StoppingNet.py
+++ StoppingNet.py
@@ -5,6 +5,7 @@ import torch
 from torch import nn
 from gbm import simulate_gbm_vectorised
 
+
 class StoppingNet(nn.Module):
     """
     Decision network for one exercise date.
@@ -93,7 +94,9 @@ def train_stopping_net(
     return net
 
 
-def price_final_date_only(S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed, continuation_val=None):
+def price_final_date_only(
+    S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed, continuation_val=None
+):
     """
     Price a Bermudan that may only be exercised at T.
 
@@ -104,7 +107,9 @@ def price_final_date_only(S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed,
     """
     n_steps = int(T / 0.002)
     S_T = simulate_gbm_vectorised(S0, r, sigma, T, n_steps, n_paths_train, seed=seed)
-    S_T = torch.tensor(S_T[:, -1], dtype=torch.float32).reshape((n_paths_train, 1)) # S_T is a torch tensor
+    S_T = torch.tensor(S_T[:, -1], dtype=torch.float32).reshape(
+        (n_paths_train, 1)
+    )  # S_T is a torch tensor
 
     logger.info(f"shape of training tensor S_T: {S_T.shape}")
 
@@ -115,7 +120,9 @@ def price_final_date_only(S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed,
         continuation = torch.zeros(n_paths_train, dtype=torch.float32)
     else:
         print(f"price_final_date, got continuation value {continuation_val}")
-        continuation = torch.full((n_paths_train,), continuation_val, dtype=torch.float32)
+        continuation = torch.full(
+            (n_paths_train,), continuation_val, dtype=torch.float32
+        )
 
     trained_net = train_stopping_net(
         StoppingNet(d=1), S_T, payoff, continuation, 0.3, n_epochs=10, batch=8192
@@ -124,16 +131,22 @@ def price_final_date_only(S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed,
     S_T_eval = simulate_gbm_vectorised(
         S0, r, sigma, T, n_steps, n_paths_eval, seed=seed
     )
-    S_T_eval = torch.tensor(S_T_eval[:,-1], dtype=torch.float32).reshape(n_paths_eval, 1) # S_T_eval is a torch tensor
+    S_T_eval = torch.tensor(S_T_eval[:, -1], dtype=torch.float32).reshape(
+        n_paths_eval, 1
+    )  # S_T_eval is a torch tensor
     standard_error = S_T_eval.std() / math.sqrt(n_paths_eval)
 
     payoff_eval = math.exp(-r * T) * torch.clamp(K - S_T_eval, min=0.0)
-   
+
     with torch.no_grad():
         result = trained_net.forward(S_T_eval)
         exercise = (result > 0.5).float()
-        continuation = torch.full((n_paths_eval,), continuation_val, dtype=torch.float32)
-        payoff_conditioned = payoff_eval.squeeze(-1) * exercise + continuation.squeeze(-1) * (1 - exercise)
+        continuation = torch.full(
+            (n_paths_eval,), continuation_val, dtype=torch.float32
+        )
+        payoff_conditioned = payoff_eval.squeeze(-1) * exercise + continuation.squeeze(
+            -1
+        ) * (1 - exercise)
 
         # logger.info(f'shape of logits result {result.shape}')
         # logger.info(f'shape of payoff_eval result {payoff_eval.shape}')
@@ -148,7 +161,9 @@ def price_final_date_only(S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed,
         positive_exercise = result > 0.5
         count_agreement = (positive_payoffs == positive_exercise).float().mean()
 
-        logger.info(f"disagreement rate between the net and analytically = {1 - count_agreement}")
+        logger.info(
+            f"disagreement rate between the net and analytically = {1 - count_agreement}"
+        )
 
         disagreement_mask = positive_payoffs != positive_exercise
         disagreement_payoffs = payoff_eval_squeezed[disagreement_mask]
@@ -168,8 +183,9 @@ def price_final_date_only(S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed,
         return mean_payoff, standard_error
 
 
-def price_bermudan_two_dates(S0, K, r, sigma, T, n_paths_train, n_paths_eval,
-                             seed_train, seed_eval, n_epochs=50):
+def price_bermudan_two_dates(
+    S0, K, r, sigma, T, n_paths_train, n_paths_eval, seed_train, seed_eval, n_epochs=50
+):
     """
     Price a Bermudan put exercisable at T/2 and T.
 
@@ -186,35 +202,39 @@ def price_bermudan_two_dates(S0, K, r, sigma, T, n_paths_train, n_paths_eval,
     Returns (price, se).
     """
 
-    S_T = simulate_gbm_vectorised(S0=S0,
-                                    r=r,
-                                    sigma=sigma,
-                                    T=T,
-                                    n_steps=2, # simulate on grid [0, T/2, T]
-                                    n_paths=n_paths_train,
-                                    seed=seed_train)
+    S_T = simulate_gbm_vectorised(
+        S0=S0,
+        r=r,
+        sigma=sigma,
+        T=T,
+        n_steps=2,  # simulate on grid [0, T/2, T]
+        n_paths=n_paths_train,
+        seed=seed_train,
+    )
 
-    f1 = math.exp(-r*T/2)*np.maximum(K - S_T[:,1], 0)
-    f2 = math.exp(-r*T)*np.maximum(K-S_T[:,2], 0)
+    f1 = math.exp(-r * T / 2) * np.maximum(K - S_T[:, 1], 0)
+    f2 = math.exp(-r * T) * np.maximum(K - S_T[:, 2], 0)
 
     S_T = torch.tensor(S_T[:, 1], dtype=torch.float32).reshape(n_paths_train, 1)
     payoff = torch.tensor(f1, dtype=torch.float32).reshape(n_paths_train, 1)
     continuation = torch.tensor(f2, dtype=torch.float32).reshape(n_paths_train, 1)
-    
+
     trained_net = train_stopping_net(
-            StoppingNet(d=1), S_T, payoff, continuation, 0.3, n_epochs=n_epochs, batch=8192
-        )
+        StoppingNet(d=1), S_T, payoff, continuation, 0.3, n_epochs=n_epochs, batch=8192
+    )
 
     with torch.no_grad():
 
         # FRESH paths
-        S_T = simulate_gbm_vectorised(S0=S0,
-                                        r=r,
-                                        sigma=sigma,
-                                        T=T,
-                                        n_steps=2, # simulate on grid [0, T/2, T]
-                                        n_paths=n_paths_eval,
-                                        seed=seed_eval)
+        S_T = simulate_gbm_vectorised(
+            S0=S0,
+            r=r,
+            sigma=sigma,
+            T=T,
+            n_steps=2,  # simulate on grid [0, T/2, T]
+            n_paths=n_paths_eval,
+            seed=seed_eval,
+        )
 
         # debugging
         # S2 = S_T[:, 2]
@@ -222,28 +242,37 @@ def price_bermudan_two_dates(S0, K, r, sigma, T, n_paths_train, n_paths_eval,
         # print(S2.mean(), S2.std())              # expect ≈ 105.13, ≈ 21.24
         # print(S_T[:, 1].mean())          # expect ≈ 102.53 at t=0.5
         # print(np.exp(-r*T), (np.maximum(K-S2,0)).mean())   # 0.9512, ≈ 5.859
-        
-        S_T1 = torch.tensor(S_T[:,1], dtype=torch.float32).reshape(n_paths_eval, 1)
-        S_T2 = torch.tensor(S_T[:,2], dtype=torch.float32).reshape(n_paths_eval, 1)
+
+        S_T1 = torch.tensor(S_T[:, 1], dtype=torch.float32).reshape(n_paths_eval, 1)
+        S_T2 = torch.tensor(S_T[:, 2], dtype=torch.float32).reshape(n_paths_eval, 1)
 
         stopping_probabilities = trained_net.forward(S_T1)
-        exercise_decision = (stopping_probabilities > 0.5).float() # 0.0 or 1.0, bool converted to float for later
+        exercise_decision = (
+            stopping_probabilities > 0.5
+        ).float()  # 0.0 or 1.0, bool converted to float for later
 
-        payoff_stop_at_T1 = math.exp(-r*T/2)*torch.clamp(K - S_T1, 0.0).squeeze(-1)
-        payoff_stop_at_T2 = math.exp(-r*T)*torch.clamp(K - S_T2, 0.0).squeeze(-1)
+        payoff_stop_at_T1 = math.exp(-r * T / 2) * torch.clamp(K - S_T1, 0.0).squeeze(
+            -1
+        )
+        payoff_stop_at_T2 = math.exp(-r * T) * torch.clamp(K - S_T2, 0.0).squeeze(-1)
 
         # sanity check - assert the network does not stop on a path where the payoff is 0
         test_condition = (exercise_decision == 1.0) & (payoff_stop_at_T1 <= 0.0)
-        assert test_condition.sum() == 0, "Weird result, where network stops at T1 when payoff at T1 is 0.0"
+        assert (
+            test_condition.sum() == 0
+        ), "Weird result, where network stops at T1 when payoff at T1 is 0.0"
 
-        payoff = payoff_stop_at_T1*exercise_decision + (1 - exercise_decision)*payoff_stop_at_T2
+        payoff = (
+            payoff_stop_at_T1 * exercise_decision
+            + (1 - exercise_decision) * payoff_stop_at_T2
+        )
 
         mean_price = payoff.mean()
-        standard_error = payoff.std()/math.sqrt(n_paths_eval)
+        standard_error = payoff.std() / math.sqrt(n_paths_eval)
 
         # debugging
         # never = payoff_stop_at_T1.mean()                            # should be ≈ 5.5735
         # always_if_itm = (payoff_stop_at_T1*(payoff_stop_at_T1>0) + payoff_stop_at_T2*(payoff_stop_at_T1<=0)).mean()   # naive rule, exercise at t1 if ITM
         # print(never, always_if_itm, mean_price)
-        
+
         return mean_price, standard_error
--- tests/test_stopping_net.py
+++ tests/test_stopping_net.py
@@ -3,9 +3,12 @@ from loguru import logger
 
 import torch
 from gbm import simulate_gbm_vectorised
-from StoppingNet import (price_final_date_only, 
-                         train_stopping_net, StoppingNet,
-                         price_bermudan_two_dates)
+from StoppingNet import (
+    price_final_date_only,
+    train_stopping_net,
+    StoppingNet,
+    price_bermudan_two_dates,
+)
 
 
 def test_stopping_net():
@@ -90,10 +93,11 @@ def test_price_final_date_different_seeds():
 
     logger.info(f"mean_payoff with seed zero {mean_payoff}")
     logger.info(f"mean_payoff with seed one {mean_payoff_one}")
-    
+
     assert abs(mean_payoff - mean_payoff_one) < 3 * (
         se + se_one
     ), "Payoff computed from trained network is not within 3*se of expected value."
+
 
 def test_continuation_value():
     S0 = 100
@@ -101,7 +105,7 @@ def test_continuation_value():
     sigma = 0.2
     T = 1.0
     K = 100
-    continuation_val = 20.0 #None # 20.0
+    continuation_val = 20.0  # None # 20.0
 
     seed = 0
     mean_payoff, se = price_final_date_only(
@@ -118,7 +122,10 @@ def test_continuation_value():
 
     logger.info(f"mean_payoff = {mean_payoff}")
 
-    assert math.isclose(mean_payoff, continuation_val), "mean_payoff doesn't match continuation value, net training is off."
+    assert math.isclose(
+        mean_payoff, continuation_val
+    ), "mean_payoff doesn't match continuation value, net training is off."
+
 
 def test_price_bermudan_two_dates():
     S0 = 100
@@ -127,28 +134,34 @@ def test_price_bermudan_two_dates():
     T = 1.0
     K = 100
 
-    mean_price, se = price_bermudan_two_dates(S0=S0, 
-                                              K=K, 
-                                              r=r, 
-                                              sigma=sigma, 
-                                              T=T, 
-                                              n_paths_train=100_000, 
-                                              n_paths_eval=100_000,
-                                              seed_train=0, 
-                                              seed_eval=1, 
-                                              n_epochs=50)
+    mean_price, se = price_bermudan_two_dates(
+        S0=S0,
+        K=K,
+        r=r,
+        sigma=sigma,
+        T=T,
+        n_paths_train=300_000,
+        n_paths_eval=300_000,
+        seed_train=0,
+        seed_eval=1,
+        n_epochs=50,
+    )
 
     # TODO - implement black-scholes put and show bermudan pv > european pv.
-    assert mean_price > 5.3, f"! bermudan PV: {mean_price} > european PV), check network stopping condition."
+    logger.info(f"mean_price = {mean_price}")
+    assert (
+        mean_price > 5.3
+    ), f"! bermudan PV: {mean_price} > european PV), check network stopping condition."
+
 
 if __name__ == "__main__":
     # DEBUGGING only - force run tests
     # test_stopping_net()
 
-    #test_price_final_date_only()
+    # test_price_final_date_only()
 
     # test_price_final_date_different_seeds()
 
-    #test_continuation_value()
+    # test_continuation_value()
 
     test_price_bermudan_two_dates()
